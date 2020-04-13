17ec60c7ddac2613093ad51477f4a74b38647a82
Mudland2721 <Mudland2721@gmail.com>
2020-04-12 22:23:14 -0600

    pass options

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -13,5 +13,15 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function getPasswordOptions() {
+  var lengthOfPass = prompt("How long of a password?");
+  var numbersInPass = prompt("Any numbers?");
+  var lowercaseInPass = prompt("Any lowercase?");
+  var uppercaseInPass = prompt("Any uppercase?");
+  var specialCharactersInPass = prompt("Any special characters?");
+
+  // var sushiType = prompt("What kind of sushi do you like?");
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
